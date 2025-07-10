190fa6f1189380bed1bf1577c04949dcbd6b26a0
Danny Heller <dheller307@outlook.com>
2025-07-09 21:54:40 -0400

    completed calculator v1

--- bmi.js
+++ bmi.js
@@ -1,8 +1,8 @@
 let meters;
-let Kilograms;
+let kilograms;
 let bmi;
 
-document.getElementById("custButton").onclick = function(){
+document.getElementById("custButton").onclick = function(event){
     event.preventDefault();
     const feet = parseFloat(document.getElementById("feet").value);
     const inches = parseFloat(document.getElementById("inches").value);
@@ -11,7 +11,15 @@ document.getElementById("custButton").onclick = function(){
     meters = totalInches * 0.0254;
     kilograms = pounds * 0.453592;
     bmi = kilograms/(meters**2);
+    bmi = Math.round(bmi*10)/10;
     document.getElementById("output").textContent = `Your Body Mass Index is ${bmi}`;
 }
-
+document.getElementById("metButton").onclick = function(event){
+    event.preventDefault();
+    const meters = parseFloat(document.getElementById("meters").value);
+    const kilograms = parseFloat(document.getElementById("kilograms").value);
+    bmi = kilograms/(meters**2);
+    bmi = Math.round(bmi*10)/10;
+    document.getElementById("output2").textContent = `Your Body  Mass Index is ${bmi}`;
+}
 
